c9f833ba96d3fbc6c19899580ea438e8700f6435
Marko Strukan <marko.strukan1@gmail.com>
2025-11-11 13:33:07 +0100

    add quiz app pt2

--- quiz_game.py
+++ quiz_game.py
@@ -1,1 +1,8 @@
 print("Welcome to my computer quiz!")
+playing = input("Do you want to play?(yes/no) ")
+
+if playing != "yes":
+    print("You have quit the game.")
+    quit()
+    
+print("Ok, let's play :) ")
